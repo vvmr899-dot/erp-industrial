75333d716a275707f13d56fac2a2675b937a51ff
Victor Alvizo <vvmr899@gmail.com>
2026-03-11 01:36:10 -0600

    actualizacion app

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://fdgdywzqaycbauveytan.supabase.co'
-const supabaseAnonKey = 'sb_publishable_GGqDbZay_02PWeDcA2E3kg_M7BAYjxE'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
